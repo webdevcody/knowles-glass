3298223f2876dff97804985710b6a52ed7466323
Web Dev Cody <webdevcody@gmail.com>
2024-03-18 12:04:36 -0400

    add basic ip limiting

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import * as wafv2 from "aws-cdk-lib/aws-wafv2";
 
 export default {
   config(_input) {
@@ -11,6 +12,38 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+      const cfnWebACL = new wafv2.CfnWebACL(stack, "MyCDKWebAcl", {
+        defaultAction: {
+          allow: {},
+        },
+        scope: "CLOUDFRONT",
+        visibilityConfig: {
+          metricName: "MetricForWebACLCDK",
+          cloudWatchMetricsEnabled: true,
+          sampledRequestsEnabled: true,
+        },
+        rules: [
+          {
+            name: "LimitRequests",
+            priority: 1,
+            action: {
+              block: {},
+            },
+            statement: {
+              rateBasedStatement: {
+                limit: 500,
+                aggregateKeyType: "IP",
+              },
+            },
+            visibilityConfig: {
+              sampledRequestsEnabled: true,
+              cloudWatchMetricsEnabled: true,
+              metricName: "LimitRequests1000",
+            },
+          },
+        ],
+      });
+
       const site = new NextjsSite(stack, "site", {
         customDomain: {
           domainName: "knowlesglassandglazing.com",
@@ -20,6 +53,9 @@ export default {
         cdk: {
           server: {
             logRetention: RetentionDays.ONE_DAY,
+          },
+          distribution: {
+            webAclId: cfnWebACL.attrArn,
           },
         },
         environment: {},
