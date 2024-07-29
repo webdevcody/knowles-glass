044091103b1e46c50b705cba4071580bcfea8842
Cody Seibert <webdevcody@gmail.com>
2024-07-29 17:38:25 -0400

    revert

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -10,38 +10,18 @@ import (
 	"github.com/webdevcody/go-mailing-list/routes"
 )
 
-func getAddr(hostname string) string {
-	addrs, err := net.LookupIP(hostname)
-	if err != nil {
-		fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
-		os.Exit(1)
-	}
-
-	var ipv4Addr string
-	for _, addr := range addrs {
-		if ipv4 := addr.To4(); ipv4 != nil {
-			ipv4Addr = ipv4.String()
-			break
-		}
-	}
-
-	if ipv4Addr == "" {
-		fmt.Printf("No IPv4 address found for hostname %s\n", hostname)
-		os.Exit(1)
-	}
-
-	addr := ipv4Addr + ":3000"
-	fmt.Printf("Resolved IPv4 address for %s: %v\n", hostname, addr)
-
-	return addr
-}
-
 func main() {
 	hostname := os.Getenv("HOSTNAME")
 	addr := os.Getenv("HTTP_LISTEN_ADDR")
 
 	if hostname != "" {
-		addr = getAddr(hostname)
+		addrs, err := net.LookupHost(hostname)
+		if err != nil {
+			fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
+			os.Exit(1)
+		}
+		addr = fmt.Sprintf("[%s]", addrs[0]) + ":3000"
+		fmt.Printf("Resolved addresses for %s: %v\n", hostname, addr)
 	}
 
 	app := fiber.New()
