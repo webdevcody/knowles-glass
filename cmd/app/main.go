1f98020f82944afdabc0656fb3e1bafd557a735c
Cody Seibert <webdevcody@gmail.com>
2024-07-29 17:08:42 -0400

    try to resolve hostname

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,29 +11,24 @@ import (
 )
 
 func main() {
+	hostname := os.Getenv("HOSTNAME")
+	addr := os.Getenv("HTTP_LISTEN_ADDR")
 
-	hostname := "knowles-glass-go.railway.internal"
-	addrs, err := net.LookupHost(hostname)
-	if err != nil {
-		fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
+	if hostname != "" {
+		addrs, err := net.LookupHost(hostname)
+		if err != nil {
+			fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
+			os.Exit(1)
+		}
+		addr = addrs[0] + ":3000"
+		fmt.Printf("Resolved addresses for %s: %v\n", hostname, addr)
 	}
-	fmt.Printf("Resolved addresses for %s: %v\n", hostname, addrs)
-
-	hostname = "knowles-glass-go"
-	addrs, err = net.LookupHost(hostname)
-	if err != nil {
-		fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
-		os.Exit(1)
-	}
-	fmt.Printf("Resolved addresses for %s: %v\n", hostname, addrs)
 
 	app := fiber.New()
 
 	app.Static("/public", "./public")
 
 	routes.RegisterRoutes(app)
-
-	addr := os.Getenv("HTTP_LISTEN_ADDR")
 
 	log.Fatal(app.Listen(addr))
 }
