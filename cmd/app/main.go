224b011bcead288bbca3afb904dd8ab5e7e5965e
Cody Seibert <webdevcody@gmail.com>
2024-07-29 16:38:13 -0400

    print

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,6 +11,22 @@ import (
 )
 
 func main() {
+
+	hostname := "knowles-glass-go.railway.internal"
+	addrs, err := net.LookupHost(hostname)
+	if err != nil {
+		fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
+	}
+	fmt.Printf("Resolved addresses for %s: %v\n", hostname, addrs)
+
+	hostname = "knowles-glass-go"
+	addrs, err = net.LookupHost(hostname)
+	if err != nil {
+		fmt.Printf("Failed to resolve hostname %s: %v\n", hostname, err)
+		os.Exit(1)
+	}
+	fmt.Printf("Resolved addresses for %s: %v\n", hostname, addrs)
+
 	app := fiber.New()
 
 	app.Static("/public", "./public")
